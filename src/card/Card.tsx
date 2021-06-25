366d62c474592892198eb6ba314bb7889d1a53b4
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-06-25 19:46:58 -0300

    feat: add new card component

---
+++ src/card/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => (
+  <div>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, delectus quos. Corrupti quo
+      sint nihil illo iure dolore soluta? Animi nihil optio deserunt quaerat eaque, aut eum dolor a.
+      Placeat.
+    </p>
+  </div>
+);
+
+export default Card;
--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -2,6 +2,6 @@ export const Config = {
   site_name: 'Coffer',
   title: 'The Coffer Hub',
   description:
-    'Grupo Mastermind onde buscamos colaborar para construir ótimas ferramentas e aplicativos.',
+    'Grupo Mastermind onde buscamos colaborar para construir ótimas ferramentas e aplicações.',
   locale: 'pt',
 };
